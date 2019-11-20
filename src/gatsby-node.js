e2c10836c1f39e3da621a01edce0943809391ef6
robmarshall <robertmarshllproductions@gmail.com>
2019-11-20 07:41:36 +0000

    Added node creation

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,5 +1,6 @@
-const chalk = require("chalk");
 const axios = require("axios");
+const chalk = require("chalk");
+const log = console.log;
 
 const {
   flattenArray,
@@ -18,11 +19,9 @@ if (activeEnv == "development") {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 }
 
-const log = console.log;
-
 exports.sourceNodes = async (
   { actions: { createNode }, createContentDigest, createNodeId },
-  { api }
+  { api, limit }
 ) => {
   log(chalk.black.bgWhite("Starting Random Cat Source plugin"));
 
@@ -37,9 +36,7 @@ exports.sourceNodes = async (
 
   try {
     result = await axios.get(
-      `https://api.thecatapi.com/v1/images/search?limit=10${
-        catCategories ? "?category_ids=" + catCategories : ""
-      }`,
+      `https://api.thecatapi.com/v1/images/search?limit=${limit ? limit : "5"}`,
       {
         responseType: "json",
         headers: { "x-api-key": api }
@@ -48,6 +45,44 @@ exports.sourceNodes = async (
   } catch (err) {
     log(chalk.bgRed("There was an error"));
     log(err);
+  }
+
+  if (result.data) {
+    for (const [key, value] of Object.entries(result.data)) {
+      let catObj = result.data[key];
+      // Add the Gatsby internal information.
+      // contentDigest tracks any changes and lets Gatsby know if
+      // the data needs to be rerendered, or is everything is
+      // the same.
+
+      console.log(catObj);
+
+      let newCatNode = {
+        // This is needed for Gatsby to track the node
+        // https://www.gatsbyjs.org/docs/node-api-helpers/#createNodeId
+        id: createNodeId(`random-cat-${catObj.id.toString()}`),
+        internal: {
+          // All nodes need a unique nodetype. It is used by GraphQL so users
+          // can query for nodes based on their type.
+          type: "Random__Cat",
+          // The contentDigest is a hashed string made from the returned object
+          // If the content changes, then the hash changes. Informing Gatsby
+          // it needs to recreate the node with new data.
+          // https://www.gatsbyjs.org/docs/node-api-helpers/#createContentDigest
+          contentDigest: createContentDigest(catObj)
+        },
+        // Here we add our cat data to the node
+        catId: parseInt(catObj.id),
+        url: catObj.url,
+        width: catObj.width,
+        height: catObj.height,
+        breeds: catObj.breeds
+      };
+
+      // Finally we pass the new cat object to createNode, to add the data to GraphQL
+      // https://www.gatsbyjs.org/docs/actions/#createNode
+      createNode(newCatNode);
+    }
   }
 
   return result;
