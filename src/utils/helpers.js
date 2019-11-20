ad11fe4245419445b2534fee4b607c3ef0d68122
robmarshall <robertmarshllproductions@gmail.com>
2019-11-20 07:41:49 +0000

    Linted

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,33 +1,33 @@
 // Destructure array and turn to string
 function flattenArray(array) {
-    return [].concat(...array)
+    return [].concat(...array);
 }
 
 // Make unix timestamp
 function getCurrentTimestamp() {
-    return Math.round(new Date().getTime() / 1000)
+    return Math.round(new Date().getTime() / 1000);
 }
 
 // Check if element is array
 function isArray(element) {
-    if (!element) return false
-    return element.constructor === Array
+    if (!element) return false;
+    return element.constructor === Array;
 }
 
 // Check if element is an object
 function isObject(element) {
-    if (!element) return false
-    if (typeof element !== 'object') return false
-    if (element instanceof Array) return false
-    return true
+    if (!element) return false;
+    if (typeof element !== 'object') return false;
+    if (element instanceof Array) return false;
+    return true;
 }
 
 // Check object key to see if it has values
 function isObjEmpty(obj) {
     for (var key in obj) {
-        if (obj.hasOwnProperty(key)) return false
+        if (obj.hasOwnProperty(key)) return false;
     }
-    return true
+    return true;
 }
 
 module.exports = {
@@ -36,4 +36,4 @@ module.exports = {
     isArray,
     isObject,
     isObjEmpty,
-}
+};
